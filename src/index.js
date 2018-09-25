2ae5ca0e450fe8037b70dad2d6cb57d970321c15
Bruno Russi Lautenschlager <bruno.lautenschlager@ciss.com.br>
2018-09-25 09:52:57 -0300

    feat: add font to project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./pages/main";
 import registerServiceWorker from "./registerServiceWorker";
+import "./client-register-font/styles.css"
 
 ReactDOM.render(<Main />, document.getElementById("root"));
 

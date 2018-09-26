01885cccbfc562e0c9b46d5e4f360676e35b16be
Bruno Russi Lautenschlager <bruno.lautenschlager@ciss.com.br>
2018-09-26 17:52:30 -0300

    feat: initial struct for redux

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./pages/main";
 import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from 'react-redux';
+import { Store } from './store';
 import "./assets/font/styles.css";
 
-ReactDOM.render(<Main />, document.getElementById("root"));
+ReactDOM.render(
+    <Provider store={Store}>
+        <Main />
+    </Provider>,
+document.getElementById("root"));
 
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { clickReducer } from './clickReducer';
+
+export const Reducers = {
+  clickState: clickReducer
+};

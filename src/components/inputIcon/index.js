b7f90b637bd3100b6a695476e99ec2b8f0861671
Bruno Russi Lautenschlager <bruno.lautenschlager@ciss.com.br>
2018-09-27 16:55:11 -0300

    feat: new component input with icon

---
+++ src/components/inputIcon/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class InputIcon extends Component {
+  handleChange = event => {
+    const text = event.target.value;
+    this.props.onChange(this.props.id, text);
+  };
+
+  render() {
+    const { type, placeholder, icon } = this.props;
+
+    return (
+      <div className="inputWithIcon">
+        <input
+          type={type}
+          placeholder={placeholder}
+          onChange={this.handleChange}
+        />
+
+        <i className={icon} aria-hidden="true"/>
+      </div>
+    );
+  }
+}
+
+export default InputIcon;

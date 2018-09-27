35526ebf108552bd7a660a66de4be1b6cf424121
Bruno Russi Lautenschlager <bruno.lautenschlager@ciss.com.br>
2018-09-27 16:55:28 -0300

    test of components

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { clickButton } from '../../actions';
 import "./styles.css";
 
 import Input from "../../components/input";
+import Header from "../../components/header";
+import ButtonIcon from "../../components/buttonIcon"
 
 class Login extends Component {
   constructor(props){
@@ -21,8 +26,17 @@ class Login extends Component {
   }
 
   render() {
+    const {
+      clickButton,
+      newValue
+    } = this.props;
+
     return (
       <div className="window-login">
+        <Header title="Teste" backTo="/"/>
+
+        <h1>{newValue}</h1>
+        <ButtonIcon text="Botão"/>
         <div className="header-login">
           <i className="icon-user-book icon-header" aria-hidden="true"/>
           <p className="header-name">Clientes</p>
@@ -30,13 +44,23 @@ class Login extends Component {
         
 
         <div className="body-login">
-          <Input type="text" placeholder="Usúario" icon="icon-group" id="usuario" onChange={this.handleFieldChange}/>
-          <Input type="password" placeholder="Senha" icon="icon-lock" id="senha" onChange={this.handleFieldChange}/>
+          <Input type="text" placeholder="Usúario" id="usuario" onChange={this.handleFieldChange}/>
+          <Input type="password" placeholder="Senha" id="senha" onChange={this.handleFieldChange}/>
         </div>
+        <button onClick={() => clickButton(this.state.usuario)}>
+          Click me!
+        </button>
       </div>
     );
   }
   
 }
 
-export default Login;
+const mapStateToProps = store => ({
+  newValue: store.clickState.newValue
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ clickButton }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);

a23a746826b74fe1cbe37ca250fea0051faccc19
Bruno Russi <brunoxd13@gmail.com>
2018-10-10 00:43:59 -0300

    feat: styled identificacao page

--- src/pages/identificacao/index.js
+++ src/pages/identificacao/index.js
@@ -1,10 +1,51 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+import Header from "../../components/header";
+import Input from "../../components/input";
+import ButtonIcon from "../../components/buttonIcon";
+
 import "./styles.css";
 
 class Identificacao extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nome: ""
+    };
+
+    this.handleFieldChange = this.handleFieldChange.bind(this);
+  }
+
+  handleFieldChange(fieldId, value) {
+    this.setState({ [fieldId]: value });
+  }
+
+  onClickBtn = () => {
+    //TODO: data validation here
+    this.props.history.push("/");
+  };
+
   render() {
-    
+    return (
+      <div className="container-identificacao">
+        <Header title="Identificação" backTo="/" />
+        <div className="container-cmp-identificacao">
+          <div className="container-input-nome">
+            <Input
+              type="text"
+              placeholder="Digite seu nome completo"
+              id="nome"
+              onChange={this.handleFieldChange}
+            />
+          </div>
+
+          <ButtonIcon text="Próximo" onClick={this.onClickBtn} />
+        </div>
+      </div>
+    );
   }
 }
 
-export default Identificacao;
+export default withRouter(Identificacao);

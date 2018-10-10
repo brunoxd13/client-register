17fe84872c8c2207a8414408724a4c65be8484ec
Bruno Russi Lautenschlager <bruno.lautenschlager@ciss.com.br>
2018-10-10 11:33:02 -0300

    fix: ajusted components

--- src/routes.js
+++ src/routes.js
@@ -3,25 +3,15 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import InformarCpf from "./pages/informarCpf";
 import Identificacao from "./pages/identificacao";
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      true ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
-      )}
-  />
-);
+import Contato from "./pages/contato";
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={() => <InformarCpf />} />
       <Route path="/identificacao" component={() => <Identificacao />} />
-
-      <Route path="*" component={() => <h1>Page not found</h1>} />
+      <Route path="/contato" component={() => <Contato />} />
+      <Route path="*" component={() => <Redirect to={{ pathname: "/" }} />} />
     </Switch>
   </BrowserRouter>
 );

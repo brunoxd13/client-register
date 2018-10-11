89ee876757675f6ca3f9544e09e32e1c75e7ca87
Bruno Russi <brunoxd13@gmail.com>
2018-10-10 23:11:10 -0300

    chore: login ajustes

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { clickButton } from '../../actions';
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { clickButton } from "../../actions";
 import "./styles.css";
 
 import Input from "../../components/input";
 import Header from "../../components/header";
-import ButtonIcon from "../../components/buttonIcon"
+import ButtonIcon from "../../components/buttonIcon";
 
 class Login extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -19,33 +20,38 @@ class Login extends Component {
 
     this.handleFieldChange = this.handleFieldChange.bind(this);
   }
-  
 
   handleFieldChange(fieldId, value) {
     this.setState({ [fieldId]: value });
   }
 
   render() {
-    const {
-      clickButton,
-      newValue
-    } = this.props;
+    const { clickButton, newValue } = this.props;
 
     return (
       <div className="window-login">
-        <Header title="Teste" backTo="/"/>
+        <Header title="Teste" backTo="/" />
 
         <h1>{newValue}</h1>
-        <ButtonIcon text="Botão"/>
+        <ButtonIcon text="Botão" />
         <div className="header-login">
-          <i className="icon-user-book icon-header" aria-hidden="true"/>
+          <i className="icon-user-book icon-header" aria-hidden="true" />
           <p className="header-name">Clientes</p>
         </div>
-        
 
         <div className="body-login">
-          <Input type="text" placeholder="Usúario" id="usuario" onChange={this.handleFieldChange}/>
-          <Input type="password" placeholder="Senha" id="senha" onChange={this.handleFieldChange}/>
+          <Input
+            type="text"
+            placeholder="Usúario"
+            id="usuario"
+            onChange={this.handleFieldChange}
+          />
+          <Input
+            type="password"
+            placeholder="Senha"
+            id="senha"
+            onChange={this.handleFieldChange}
+          />
         </div>
         <button onClick={() => clickButton(this.state.usuario)}>
           Click me!
@@ -53,7 +59,6 @@ class Login extends Component {
       </div>
     );
   }
-  
 }
 
 const mapStateToProps = store => ({
@@ -63,4 +68,4 @@ const mapStateToProps = store => ({
 const mapDispatchToProps = dispatch =>
   bindActionCreators({ clickButton }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Login));

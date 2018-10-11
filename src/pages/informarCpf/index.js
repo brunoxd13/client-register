1b450e306133d18cf5c4b0fe60ceb97920b6a04e
Bruno Russi <brunoxd13@gmail.com>
2018-10-10 23:20:42 -0300

    feat: redux on cpfCnpj

--- src/pages/informarCpf/index.js
+++ src/pages/informarCpf/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { clickCpfCnpj } from "../../actions";
+
 import InputIcon from "../../components/inputIcon";
 import ButtonIcon from "../../components/buttonIcon";
-import { withRouter } from "react-router-dom";
 
 import "./styles.css";
 
@@ -22,20 +27,22 @@ class InformarCpf extends Component {
     this.setState({ [fieldId]: value });
   }
 
-  onClickBtn = () => {
-    //TODO: data validation here
-    this.props.history.push("/identificacao");
-  };
-
   //TODO: on component did mount get logo image;
 
   render() {
+    const { clickCpfCnpj } = this.props;
+
+    const onClickBtn = () => {
+      //TODO: data validation here
+      clickCpfCnpj(this.state.cpfCnpj);
+      this.props.history.push("/identificacao");
+    };
+
     return (
       <div className="container-informar-cpf">
         <div className="container-img">
           <img className="logo-img" src={logo} alt="Logo" />
         </div>
-
         <div className="containerCmpInformarCpf">
           <div className="container-input-cpf-cnpj">
             <InputIcon
@@ -48,7 +55,7 @@ class InformarCpf extends Component {
             />
           </div>
           <div>
-            <ButtonIcon text="Próximo" onClick={this.onClickBtn} />
+            <ButtonIcon text="Próximo" onClick={onClickBtn} />
           </div>
         </div>
       </div>
@@ -56,4 +63,13 @@ class InformarCpf extends Component {
   }
 }
 
-export default withRouter(InformarCpf);
+const mapStateToProps = store => ({
+  cpfCnpj: store.clickCliente.cpfCnpj
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ clickCpfCnpj }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  withRouter(InformarCpf)
+);

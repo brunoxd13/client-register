bf54a4d93a0e76cc2d0daa7021655f8ac6324381
Bruno Russi Lautenschlager <bruno.lautenschlager@ciss.com.br>
2018-10-18 17:23:18 -0300

    feat: api service impemented

---
+++ src/service/api.js
@@ -0,0 +1,44 @@
+const api = "";
+
+const headers = {
+  Accept: "application/json"
+};
+
+export const getClienteByCpfCnpj = cpfCnpj =>
+  fetch(`${api}/cliente/cpfcnpj/${cpfCnpj}`, { headers })
+    .then(res => res.json())
+    .then(data => data);
+
+export const getCidadeByCep = cep =>
+  fetch(`${api}/cidade/cep/${cep}`, { headers })
+    .then(res => res.json())
+    .then(data => data);
+
+export const getImagemLogo = () =>
+  fetch(`${api}/imagem/logo`, { headers })
+    .then(res => res.json())
+    .then(data => data);
+
+export const createCliente = cliente =>
+  fetch(`${api}/cliente/`, {
+    method: "POST",
+    headers: {
+      ...headers,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ cliente })
+  })
+    .then(res => res.json())
+    .then(data => data);
+
+export const updateCliente = cliente =>
+  fetch(`${api}/cliente/${cliente.id}`, {
+    method: "PUT",
+    headers: {
+      ...headers,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ cliente })
+  })
+    .then(res => res.json())
+    .then(data => data);

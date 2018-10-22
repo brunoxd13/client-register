89c9487ea5425146c516eabec364aa1a25ffe99d
Bruno Russi Lautenschlager <bruno.lautenschlager@ciss.com.br>
2018-10-22 17:37:06 -0300

    chore: log changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,8 @@
-import { createStore } from 'redux';
-import { Reducers } from '../reducers';
+import { createStore } from "redux";
+import { Reducers } from "../reducers";
 
 export const Store = createStore(Reducers);
+
+Store.subscribe(() => {
+  console.log("The new state is: ", Store.getState());
+});

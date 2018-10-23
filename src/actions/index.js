b61b96315f168526d297ced53303362fda4698a6
Bruno Russi <brunoxd13@gmail.com>
2018-10-22 22:12:39 -0300

    refactor: removed un used actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,35 +1,4 @@
-import {
-  CLICK_CPF_CNPJ,
-  CLICK_IDENTIFICACAO,
-  CLICK_CONTATO,
-  CLICK_ENDERECO,
-  UPDATE_CLIENTE
-} from "./actionTypes";
-
-export const clickCpfCnpj = cpfCnpj => ({
-  type: CLICK_CPF_CNPJ,
-  newCpfCnpj: cpfCnpj
-});
-
-export const clickIdentificacao = nome => ({
-  type: CLICK_IDENTIFICACAO,
-  newNome: nome
-});
-
-export const clickContato = (celular, email, dtNascimento) => ({
-  type: CLICK_CONTATO,
-  newCelular: celular,
-  newEmail: email,
-  newDtNascimento: dtNascimento
-});
-
-export const clickEndereco = (cep, cidade, rua, bairro) => ({
-  type: CLICK_ENDERECO,
-  newCep: cep,
-  newCidade: cidade,
-  newRua: rua,
-  newBairro: bairro
-});
+import { UPDATE_CLIENTE } from "./actionTypes";
 
 export const updateCliente = cliente => ({
   type: UPDATE_CLIENTE,

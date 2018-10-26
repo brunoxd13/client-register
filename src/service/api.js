3d11448967b29228af2a43dd897d914a810e1129
Bruno Russi Lautenschlager <bruno.lautenschlager@ciss.com.br>
2018-10-26 17:59:43 -0300

    feat: ajustado object envelop

--- src/service/api.js
+++ src/service/api.js
@@ -1,4 +1,5 @@
-const api = "";
+const api = "http://boxcontroller.ciss.com.br/cadastro-cliente";
+// http://172.16.103.94:4664/cadastro-cliente
 
 const headers = {
   Accept: "application/json"
@@ -6,7 +7,13 @@ const headers = {
 
 export const getClienteByCpfCnpj = cpfCnpj =>
   fetch(`${api}/cliente/cpfcnpj/${cpfCnpj}`, { headers })
-    .then(res => res.json())
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error("Something went wrong ...");
+      }
+    })
     .then(data => data)
     .catch(error => {
       console.warn(error);
@@ -35,7 +42,7 @@ export const createCliente = cliente =>
       ...headers,
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ cliente })
+    body: JSON.stringify(cliente)
   })
     .then(res => res.json())
     .then(data => data)
@@ -44,13 +51,13 @@ export const createCliente = cliente =>
     });
 
 export const updateCliente = cliente =>
-  fetch(`${api}/cliente/${cliente.id}`, {
+  fetch(`${api}/cliente/${cliente.idCliente}`, {
     method: "PUT",
     headers: {
       ...headers,
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ cliente })
+    body: JSON.stringify(cliente)
   })
     .then(res => res.json())
     .then(data => data)

47391ee94b2daeac850edfa389d5cb3f0b86efe5
Bruno Russi <brunoxd13@gmail.com>
2018-10-30 07:37:56 -0300

    fix: routes

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import InformarCpf from "./pages/informarCpf";
 import Identificacao from "./pages/identificacao";
 import Contato from "./pages/contato";
 import Endereco from "./pages/endereco";
+import Final from "./pages/final";
 // import Login from "./pages/login";
 
 const Routes = () => (
@@ -14,6 +15,7 @@ const Routes = () => (
       <Route path="/identificacao" component={() => <Identificacao />} />
       <Route path="/contato" component={() => <Contato />} />
       <Route path="/endereco" component={() => <Endereco />} />
+      <Route path="/final" component={() => <Final />} />
       {/*<Route path="/log" component={() => <Login />} />*/}
 
       <Route path="*" component={() => <Redirect to={{ pathname: "/" }} />} />
